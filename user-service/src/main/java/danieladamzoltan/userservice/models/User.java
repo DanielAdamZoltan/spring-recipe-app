6042294ddaf5e4e55fa9e4b6a0f32eb85a49d650
DanielAdamZoltan <69815516+DanielAdamZoltan@users.noreply.github.com>
2022-02-08 02:07:36 +0100

    version 0.0.4

---
+++ user-service/src/main/java/danieladamzoltan/userservice/models/User.java
@@ -0,0 +1,35 @@
+package danieladamzoltan.userservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private String id;
+
+    @Column(name = "user_first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "user_last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "logged_in")
+    private boolean loggedIn;
+}
