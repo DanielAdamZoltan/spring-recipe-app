cd37f1776d80f51178267057811066d82087e2b5
DÁNIEL ÁDÁM ZOLTÁN <daniel.adam995@gmail.com>
2022-02-21 23:16:42 +0100

    version 0.0.16.1

--- user-service/src/main/java/danieladamzoltan/userservice/resources/AuthController.java
+++ user-service/src/main/java/danieladamzoltan/userservice/resources/AuthController.java
@@ -1,7 +1,11 @@
 package danieladamzoltan.userservice.resources;
 
+import danieladamzoltan.userservice.jwt.util.JwtUtil;
+import danieladamzoltan.userservice.repositories.RoleRepository;
+import danieladamzoltan.userservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/authentication")
 public class AuthController {
 
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
     @Autowired
-     AuthenticationManager authenticationManager;
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
 }
--- user-service/src/main/java/danieladamzoltan/userservice/security/WebSecurityConfig.java
+++ user-service/src/main/java/danieladamzoltan/userservice/security/WebSecurityConfig.java
@@ -6,10 +6,16 @@ import danieladamzoltan.userservice.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -36,8 +42,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new JwtRequestFilter();
     }
 
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+    }
 
-//    https://www.bezkoder.com/spring-boot-jwt-authentication/
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
-//    https://www.bezkoder.com/angular-spring-boot-jwt-auth/
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(32);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(authEntryPointJwt).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers("/authentication/**").permitAll()
+                .antMatchers("/recipe/**").permitAll()
+                .anyRequest().authenticated();
+        http.addFilterBefore(jwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
 }
